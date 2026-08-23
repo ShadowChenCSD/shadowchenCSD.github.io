e99c124a3b7ee96409d4a13b001435e28951af1b
ShadowChenCSD <csd_xianjian@qq.com>
2026-08-23 19:03:11 +0800

    个性化内容更新： 1. 删除一篇日记 2. 增加3款游戏：抢滩登陆战、暴力摩托、植物大战僵尸：重植版 3. 更新部分游戏的描述内容和状态

--- src/data/diary.ts
+++ src/data/diary.ts
@@ -14,12 +14,6 @@ export interface DiaryItem {
 // 示例日记数据
 const diaryData: DiaryItem[] = [
 	{
-		id: 16,
-		content: "🛠️ 2026-08-14：获得ChatGPT Plus一个月的免费使用额度！😍",
-		date: "2026-08-14T19:30:00+08:00",
-		images: ["/images/diary/ChatGPT Plus Free.png"],
-	},
-	{
 		id: 15,
 		content: "🎮 2026-08-14：《阴阳师》，PC端推送强制升级！退出、升级，必须二选一！😤",
 		date: "2026-08-14T19:30:00+08:00",
