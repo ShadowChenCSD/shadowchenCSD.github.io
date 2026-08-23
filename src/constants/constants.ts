2ad13baeec395f052c64be7b7ec161b354e801fc
ShadowChenCSD <csd_xianjian@qq.com>
2026-08-23 23:25:09 +0800

    个性化配置： 1. 删除番剧：秦时明月之龙腾万里 2. 在日记中，如果是单张图片，就缩放至75%显示，以免图片过大 3. 禁用了看板娘 4. 博客内容宽带调整为屏幕的85%（2K分辨率、100%缩放，显示效果较好）

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -13,8 +13,8 @@ export const BANNER_HEIGHT_HOME = BANNER_HEIGHT + BANNER_HEIGHT_EXTEND;
 // The height the main panel overlaps the banner, unit: rem
 export const MAIN_PANEL_OVERLAPS_BANNER_HEIGHT = 3.5;
 
-// Page width: rem
-export const PAGE_WIDTH = 100;
+// Page width: CSS length
+export const PAGE_WIDTH = "85%";
 
 // Category constants
 export const UNCATEGORIZED = "uncategorized";
